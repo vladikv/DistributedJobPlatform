421712bbdfe557adbdddb3233c3374aaf3ae2c30
vladik <121016003+vladikv@users.noreply.github.com>
2026-09-14 14:22:08 +0200

    Initial multi-module setup: job-service and worker-service with shared parent pom

---
+++ job-service/src/main/java/com/distributedjobplatform/distributedjobplatform/DistributedJobPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.distributedjobplatform.distributedjobplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedJobPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedJobPlatformApplication.class, args);
+    }
+
+}
---
+++ job-service/src/test/java/com/distributedjobplatform/distributedjobplatform/DistributedJobPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedjobplatform.distributedjobplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedJobPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
